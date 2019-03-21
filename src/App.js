29a46dddec8f36789d3a8bb4c9e068ee07556009
Hung Do <doyouknowed@gmail.com>
2019-03-21 15:14:36 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-         <p> Hello World! </p>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
     );
