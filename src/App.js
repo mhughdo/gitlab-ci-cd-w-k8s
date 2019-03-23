934c98ae724afb648a322d51430335cc38ad7c53
Hung Do <doyouknowed@gmail.com>
2019-03-23 20:49:58 +0700

    livenessprobe test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
             <div className='App'>
                 <header className='App-header'>
                     <img src={logo} className='App-logo' alt='logo' />
-                    <p>Night test!</p>
+                    <p>LivenessProbe test!</p>
                 </header>
             </div>
         )
