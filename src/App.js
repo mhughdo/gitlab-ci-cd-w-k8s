bf42aee1378a8dcc2689deef53733d7c0d940968
Hung Do <doyouknowed@gmail.com>
2019-03-23 10:42:02 +0700

    morning test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Test part 4!</p>
+          <p>Morning test!</p>
         </header>
       </div>
     );
